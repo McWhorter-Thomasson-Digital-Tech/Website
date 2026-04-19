1b2f8abb837da2e908afeaf625db785b6cd5ad25
McWhorter, Matthew (Broadcast - Engineering) <mmcwhorter6@liberty.edu>
2026-04-19 14:49:32 -0400

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import JsonLd from "../components/seo/JsonLd";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: {
@@ -33,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-scroll-behavior="smooth">
+    <html lang="en" data-scroll-behavior="smooth" className={cn("font-sans", geist.variable)}>
       <head>
         <JsonLd />
       </head>
